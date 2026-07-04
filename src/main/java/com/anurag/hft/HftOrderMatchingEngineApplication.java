7dbacd4024ead5a5a65c5ec2daa0affeff7bdb76
Anurag Nikumbh <mca-reg202527106@timscdrmumbai.in>
2026-07-05 01:09:27 +0530

    setup: initialize Spring Boot project

---
+++ src/main/java/com/anurag/hft/HftOrderMatchingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.hft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HftOrderMatchingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HftOrderMatchingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anurag/hft/HftOrderMatchingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.hft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HftOrderMatchingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
